a88f9348db243743682df0c0f50a5c423bb4b2ac
Titanina <111214348+Titanina@users.noreply.github.com>
2023-01-26 16:44:37 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,57 @@
-const http = require("http");
-const app = http.createServer((request, response) => {
-  const _url = request.url;
-  const fullUrl = new URL("http://localhost:3000" + _url);
-  const pathName = fullUrl.pathname;
-  
-  if (pathName === "/") {
-    response.writeHead(200, { "Content-Type": "text/html;charset= utf-8" });
-    response.end(`<h1>시바견은 귀엽다</<h1>`);
-  }
-});
+const express = require('express')
+const app = express()
+const mysql = require("mysql");
+const port = 3000;
+
+app.get('/', (req, res) => res.send('<h1>Hello Shiba World!<h1>'));
+
+
 
 app.listen(3000, () => {
   //포트번호 3000으로 서버 구동
-  console.log("서버 시작 주소: http:localhost:3000");
+  console.log("백엔드 서버를 동작합니다.");
+
+const mysqlConnecte = mysql.createConnection({
+
+  host : "localhost",
+  port : "3000",
+  user : "root",
+  password : "120264",
+  database : "study"
+
+
 });
- 
+
+mysqlConnecte.connect(error => {
+
+  if(error) {
+
+    console.log("Data Base 연결에 실패했습니다. Error 내용 : ", error);
+  } //if(error ) 끝
+
+  else {
+    console.log("Data Base 연결에 성공하였습니다!");
+
+}
+})
+
+// table 생성
+sql = "create table test_board(\
+    no int auto_increment primary key,\
+    title varchar(30) not null,\
+    content varchar(100) not null\
+    )"
+
+  mysqlConnecte.query(sql, (error, result) => {
+    if(error) {
+        console.log("Table 생성에 실패했습니다. Error 내용: ", error);
+    } // if(error) 끝
+    else {console.log("Table 생성에 성공하였습니다!", result);
+  }
+
+  })
+
+
+
+});
+
