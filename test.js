b0b94338caaf661b23c040909187234457e0564c
Titanina <111214348+Titanina@users.noreply.github.com>
2023-02-18 19:09:49 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,22 @@
-const express = require("express");
-const app = express();
-const dotenv =require("dotnenv").config();
-const mysqlConnectionObject = require("./config/mysql.js");
-const database = mysqlConnectionObject.init();
+const express = require('express')
+const mysql = require('mysql')
+const app = express()
+const port = 3000
+const dotenv = require('dotenv').config()
+const mysqlConObj = require('./config/mysql')
+const db = mysqlConObj.init()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}))
 
-const port = 3000;
 
-mysqlConnectionObject.open(database);
 
-app.get("/", (request, response) => response.send("<h1>Hello ShibaWorld</h1>"));
+app.post('/', (req,res) => res.send('<h1>ShibaWorld</h1>'))
 
-app.listen(port, () =>console.log("서버를 동작합니다."));
+app.listen(port, () => console.log('서버가 작동중입니다.'))
 
+app.post('localhost:3000', function(요청, 응답){
+  console.log(요청.body.title);
+  응답.send('전송완료')
+});
 
