d24b304db09624b9b064ded6c2ae4bb225b8f5e0
Titanina <111214348+Titanina@users.noreply.github.com>
2023-03-26 22:49:30 +0900

    Update main.js

--- testfolder/main.js
+++ testfolder/main.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 var url =  require('url');
+var qs = require('querystring');
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
   return `
   <!doctype html>
   <html>
@@ -13,7 +14,7 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create">create</a>
+    ${control}
     ${body}
   </body>
   </html> 
@@ -43,33 +44,38 @@ var app = http.createServer(function(request, response){
             var title = 'Welcome';
             var description = 'Hello, Node.js';
             var list = templateList(filelist);
-            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            var template = templateHTML(title, list, 
+              `<h2>${title}</h2>${description}`,      
+              `<a href="/create">create</a>`
+             
+        );
               response.writeHead(200);
               response.end(template);
       
-        })
-
-
+            });
       } else {
-
         fs.readdir('./data', function(error, filelist){
         fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
           var title = queryData.id;
           var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          var template = templateHTML(title, list,
+             `<h2>${title}</h2>${description}`,
+             `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+             );
           response.writeHead(200);
           response.end(template);
       
       });
       });
+     }
     }
-    }else if(pathname === '/create'){
+    else if(pathname === '/create'){
       fs.readdir('./data', function(error, filelist){
         var title = 'WEB - create';
         var list = templateList(filelist);
         var template = templateHTML(title, list, `
         
-          <form action="http://localhost:3000/processcreate" method="post">
+          <form action="http://localhost:3000/create_process" method="post">
           <p><input type="text" name="title" placeholder="title"></p>
           <p>
           <textarea name="description" placeholder="description"></textarea>
@@ -80,15 +86,52 @@ var app = http.createServer(function(request, response){
           </form>
         
         
-        `);
+        `, '');
           response.writeHead(200);
           response.end(template);
   
-    })
+    });
 
-    }
+    } else if(pathname === '/create_process') {
+      var body = '';
+      request.on('data', function(data){
+        body = body + data;
 
-      else {
+
+      });
+
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        fs.writeFile(`data/${title}`, description, 'utf8', 
+        function(err){
+           response.writeHead(302, {Location: `/?id=${title}`});
+          response.end();
+      })
+    });
+
+    } else if(pathname === '/update'){
+
+      fs.readdir('./data', function(error, filelist){
+        fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list,
+             `<h2>${title}</h2>${description}`,
+             `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+             );
+          response.writeHead(200);
+          response.end(template);
+
+
+
+
+
+
+      });
+      });
+    }   else {
       response.writeHead(404);
       response.end('Not found');
 
