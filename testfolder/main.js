ed61aba42eef606e7c704622f59d4d11a882b113
Titanina <111214348+Titanina@users.noreply.github.com>
2023-04-09 19:21:31 +0900

    Update main.js

--- testfolder/main.js
+++ testfolder/main.js
@@ -46,8 +46,7 @@ var app = http.createServer(function(request, response){
             var list = templateList(filelist);
             var template = templateHTML(title, list, 
               `<h2>${title}</h2>${description}`,      
-              `<a href="/create">create</a>`
-             
+              `<a href="/create">create</a>`   
         );
               response.writeHead(200);
               response.end(template);
@@ -75,7 +74,7 @@ var app = http.createServer(function(request, response){
         var list = templateList(filelist);
         var template = templateHTML(title, list, `
         
-          <form action="http://localhost:3000/create_process" method="post">
+          <form action="/create_process" method="post">
           <p><input type="text" name="title" placeholder="title"></p>
           <p>
           <textarea name="description" placeholder="description"></textarea>
@@ -111,34 +110,62 @@ var app = http.createServer(function(request, response){
       })
     });
 
+    
     } else if(pathname === '/update'){
-
-      fs.readdir('./data', function(error, filelist){
+         fs.readdir('./data', function(error, filelist){
         fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
           var title = queryData.id;
           var list = templateList(filelist);
           var template = templateHTML(title, list,
-             `<h2>${title}</h2>${description}`,
+            `
+            <form action="/update_process" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+            <p>
+            <textarea name="description" placeholder="description">${description}</textarea>
+            </p>
+            <p>
+            <input type="submit">
+            </p>
+            </form>
+            `,
+          
              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
              );
           response.writeHead(200);
           response.end(template);
-
-
-
-
-
-
+      
       });
       });
-    }   else {
+    }  
+   
+   else if(pathname === '/update_process'){
+    var body = '';
+    request.on('data', function(data){
+      body = body + data;
+
+
+    });
+
+    request.on('end', function(){
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`, function(error){
+        fs.writeFile(`data/${title}`, description, 'utf8', 
+      function(err){
+         response.writeHead(302, {Location: `/?id=${title}`});
+        response.end();
+    })
+      })
+ 
+  });
+    
+   } else {
       response.writeHead(404);
       response.end('Not found');
-
     }
- 
-  } 
-
-);
+  });
 
 app.listen(3000, () => console.log("서버 작동중입니다."));
